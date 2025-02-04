263f108153cad2ea5be03d9f0eda696f51bdc714
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-04 12:52:33 +0200

    update index.html title and add styling; integrate Prettier and Tailwind CSS plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
+import React from 'react'
+import './App.css';
 
-function App() {
-
+const App = () => {
   return (
     <>
-
+      <div className='flex w-[640px] flex-col py-4 mx-auto'>
+        <div className="flex flex-col gap-2 rounded-xl bg-white p-4">
+          {/* header */}
+          <header>
+            <h1 className="mb-4 border-b border-gray-300 pb-3 text-2xl font-bold">
+              Journal App
+            </h1>
+            <p>
+              Embrace each day with reflection: Capture your moments, chart your growth, and
+              craft your journey, one story at a time.
+            </p>
+          </header>
+        </div>
+        {/* tabs */}
+        {/* from */}
+      </div>
     </>
   )
 }
