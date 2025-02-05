f277658a9cc2ff5ab9a5e260e2e567d6ef8de3a7
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-05 11:32:46 +0200

    add journal entry form and enhance tab styles; implement input fields and button

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -8,11 +8,11 @@ const Tabs = () => {
             <div role="tablist" className="tabs tabs-boxed tabs-xl">
                 <a
                     role="tab"
-                    className={`tab ${selectedTab === 'add' ? 'tab-active font-bold' : ''}`}
+                    className={`tab ${selectedTab === 'add' ? 'tab-active font-bold bg-red-200 border border-2xl' : ''}`}
                     onClick={() => setSelectedTab('add')}
                 >Add Entry</a>
                 <a role="tab"
-                    className={`tab ${selectedTab === 'entries' ? 'tab-active font-bold' : ''}`}
+                    className={`tab ${selectedTab === 'entries' ? 'tab-active font-bold bg-red-200 border border-2xl' : ''}`}
                     onClick={() => setSelectedTab('entries')}
                 >Journal Entries</a>
             </div>
